b52635a7205cb8ac855088910bf9cdb1fc7ad322
Erich Malfertheiner <erichmalfertheiner01@gmail.com>
2021-02-18 01:21:10 -0300

    pushed directories to github

---
+++ src/primeiraMediaNota/CalculoNota.java
@@ -0,0 +1,40 @@
+package primeiraMediaNota;
+
+public class CalculoNota {
+	
+	// technique: breaking code into small pieces
+	// more easy to view all code (when together)
+	
+	
+	public static void main(String[] args) {
+		
+		
+		// step 1: media semester equal to media of bimester notes
+		System.out.println("step 1");
+		
+		
+		
+		// step 2: bimester notes weights: exam(3), project(3), exercises(2) and colaboration (3)
+		// obs: based on values, maximum notes exceeds maximum value (10), so if a student delivered all, he can obtain 11 as note e.g
+		System.out.println("step 2");
+		
+		
+		
+		// step 3: need data input by keyboard
+		System.out.println("step 3");
+		
+		
+		
+		// step 4: approval notes >= 8
+		System.out.println("step 4");
+		
+		
+		
+		// step 5: repproval notes < 8, how much note need, base on 10 as maximmum note (10 minus medi e.g 8 - 10 = -2)
+		System.out.println("step 5");
+		
+		
+		
+		
+	}
+}
